fb476f764fe9ff93b168767e5e173690fc830a6a
Nobuyuki Hiwatashi <hiwanobu@gmail.com>
2022-04-12 23:40:24 +0900

    init

---
+++ app/Policies/PostPolicy.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class PostPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any models.
+     *
+     * @param  \App\Models\User|null  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function viewAny(?User $user)
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     *
+     * @param  \App\Models\User|null  $user
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function view(?User $user, Post $post)
+    {
+        if ($post->published_at) {
+            return true;
+        }
+
+        return $user && $user->is($post->author);
+    }
+
+    /**
+     * Determine whether the user can view the post's author.
+     *
+     * @param  \App\Models\User|null  $user
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function viewAuthor(?User $user, Post $post)
+    {
+        return $this->view($user, $post);
+    }
+
+    /**
+     * Determine whether the user can view the post's comments.
+     *
+     * @param  \App\Models\User|null  $user
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function viewComments(?User $user, Post $post)
+    {
+        return $this->view($user, $post);
+    }
+
+    /**
+     * Determine whether the user can view the post's tags.
+     *
+     * @param  \App\Models\User|null  $user
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function viewTags(?User $user, Post $post)
+    {
+        return $this->view($user, $post);
+    }
+
+    /**
+     * Determine whether the user can create models.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function create(User $user)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function update(User $user, Post $post)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function delete(User $user, Post $post)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function restore(User $user, Post $post)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function forceDelete(User $user, Post $post)
+    {
+        //
+    }
+}
